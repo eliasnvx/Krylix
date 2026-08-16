40b47f3d79098158a9212f2468c762cdeac21033
Elias <elias.nevx@gmail.com>
2026-08-16 16:30:50 +0300

    Update CI workflows for Java 21 and multi-loader architecture, bump version to 1.2.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,19 +7,19 @@ const val minecraftVersion = "1.21.1"
 // Mod
 const val modId = "krylix"
 const val modGroup = "com.eliasnvx"
-const val coreVersion = "1.1.0"
+const val coreVersion = "1.2.0"
 
 // NeoForge
 const val neoForgeVersion = "21.1.77" // https://projects.neoforged.net/neoforged/neoforge
 const val neoModDevPlugin = "2.0.78" // https://projects.neoforged.net/neoforged/moddevgradle
-const val neoForgeModVersion = "1.1.0-MC$minecraftVersion"
+const val neoForgeModVersion = "1.2.0-MC$minecraftVersion"
 const val neoForgeModArchive = "$modId-$neoForgeModVersion-neoforge"
 
 // Fabric
 const val fabricLoomVersion = "1.7.4"
 const val fabricLoaderVersion = "0.16.5"
 const val fabricApiVersion = "0.104.0+1.21.1"
-const val fabricModVersion = "1.1.0-MC$minecraftVersion"
+const val fabricModVersion = "1.2.0-MC$minecraftVersion"
 const val fabricModArchive = "$modId-$fabricModVersion-fabric"
 
 // External dependencies
